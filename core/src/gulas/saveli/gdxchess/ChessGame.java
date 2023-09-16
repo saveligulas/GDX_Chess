602ca1729f4954fb8793c4b96fbeb9a26247dcdf
saveligulas <saveli.gulas@gmail.com>
2023-09-16 22:13:00 +0200

    implemented new image

--- core/src/gulas/saveli/gdxchess/ChessGame.java
+++ core/src/gulas/saveli/gdxchess/ChessGame.java
@@ -12,7 +12,7 @@ public class ChessGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("Cat03.jpg");
 	}
 
 	@Override

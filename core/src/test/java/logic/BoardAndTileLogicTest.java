ac37ddb9f4cc68abb7e34ecf7ac6deb9cc943f06
saveligulas <saveli.gulas@gmail.com>
2023-09-17 14:57:44 +0200

    added test file

---
+++ core/src/test/java/logic/BoardAndTileLogicTest.java
@@ -0,0 +1,30 @@
+package logic;
+
+import board.ChessBoard;
+import board.LogicTile;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BoardAndTileLogicTest {
+    @Test
+    public void boardAndTileSetCalculationTest() {
+        ChessBoard board = new ChessBoard();
+        byte correctTilesFound = 0;
+
+        for(LogicTile logicTile : board.getLogicTiles()) {
+            byte x = logicTile.getX_coordinate();
+            byte y = logicTile.getY_coordinate();
+
+            byte calIndex = LogicTile.getIndex(x, y);
+            LogicTile calLogicTile = board.getLogicTiles()[calIndex];
+            if (calLogicTile.equals(logicTile)) {
+                correctTilesFound++;
+            }
+        }
+        assertEquals(64, correctTilesFound);
+    }
+
+    @Test
+    public void
+}

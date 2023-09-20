b447c68f9b0ad7ba0e386cd098e57e743ebe5f0f
saveligulas <saveli.gulas@gmail.com>
2023-09-20 16:08:18 +0200

    added testing body

--- core/src/test/java/logic/PieceMovementLogicTest.java
+++ core/src/test/java/logic/PieceMovementLogicTest.java
@@ -1,7 +1,24 @@
 package logic;
 
 import board.ChessBoard;
+import board.LogicTile;
+import model.Piece_Type;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PieceMovementLogicTest {
-    ChessBoard board = new ChessBoard();
+    private static ChessBoard chessBoard;
+
+    @BeforeClass
+    public static void setUpChessBoard() {
+        chessBoard = new ChessBoard();
+    }
+
+    @Test
+    public void testKingPosition() {
+        LogicTile[] tiles = chessBoard.getLogicTiles();
+        assertEquals(tiles[4].getPieceOnTile().getType(), Piece_Type.KING);
+    }
 }

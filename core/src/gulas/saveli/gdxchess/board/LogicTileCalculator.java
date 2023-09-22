bb406060b1648f650a156a5043d035f5743cc317
saveligulas <saveli.gulas@gmail.com>
2023-09-22 17:00:44 +0200

    added new class to move LogicTile methods

---
+++ core/src/gulas/saveli/gdxchess/board/LogicTileCalculator.java
@@ -0,0 +1,11 @@
+package board;
+
+public class LogicTileCalculator {
+    public static byte[] getCoordinatesXandY(byte index) {
+
+    }
+
+    public static byte getIndex(byte x, byte y) {
+
+    }
+}

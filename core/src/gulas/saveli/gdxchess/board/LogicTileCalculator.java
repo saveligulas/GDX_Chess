6226fb12e8faaff7921f289df51dd0f6c8308154
saveligulas <saveli.gulas@gmail.com>
2023-09-25 23:51:23 +0200

    moved methods

--- core/src/gulas/saveli/gdxchess/board/LogicTileCalculator.java
+++ core/src/gulas/saveli/gdxchess/board/LogicTileCalculator.java
@@ -2,10 +2,17 @@ package board;
 
 public class LogicTileCalculator {
     public static byte[] getCoordinatesXandY(byte index) {
+        int row = index / 8;
+        int col = index % 8;
 
+        byte[] coordinates = new byte[2];
+        coordinates[0] = (byte) col;
+        coordinates[1] = (byte) row;
+
+        return coordinates;
     }
 
     public static byte getIndex(byte x, byte y) {
-
+        return (byte) (y * 8 + x);
     }
 }

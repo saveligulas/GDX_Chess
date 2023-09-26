5de2794ad089662ee4f54838a3bad131721e57f5
saveligulas <saveli.gulas@gmail.com>
2023-09-26 18:11:10 +0200

    added fields and builder annotation

--- core/src/test/java/logic/MoveData.java
+++ core/src/test/java/logic/MoveData.java
@@ -1,9 +1,16 @@
 package logic;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class MoveData {
 
+    private boolean upwards;
+    private boolean leftwards;
+    private byte range;
+    private byte startIndex;
+    private byte endIndex;
 
 }

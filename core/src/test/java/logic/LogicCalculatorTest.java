09d39637884c8bbda9d0a027f5f2f09bc60d1024
saveligulas <saveli.gulas@gmail.com>
2023-09-26 16:10:11 +0200

    added new test class

---
+++ core/src/test/java/logic/LogicCalculatorTest.java
@@ -0,0 +1,28 @@
+package logic;
+
+import board.LogicTileCalculator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LogicCalculatorTest {
+    @Test
+    public void testGetIndex() {
+        byte[] correctAnswers = new byte[64];
+        byte[] calculatedAnswers = new byte[64];
+
+        for (byte i = 0; i < 64; i++) {
+            correctAnswers[i] = i;
+        }
+
+        for (byte rows = 0; rows < 8; rows++) {
+            for (byte columns = 0; columns < 8; columns++) {
+                calculatedAnswers[LogicTileCalculator.getIndex(columns, rows)] = LogicTileCalculator.getIndex(columns, rows);
+            }
+        }
+
+        for (byte k = 0; k < 64; k++) {
+            assertEquals(calculatedAnswers[k], correctAnswers[k]);
+        }
+    }
+}

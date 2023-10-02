0bc75c4b9fde28c91cad72da8cc014db394e3161
saveligulas <saveli.gulas@gmail.com>
2023-10-02 07:41:32 +0200

    added new data class

---
+++ core/src/gulas/saveli/gdxchess/model/LogicTileMoveData.java
@@ -0,0 +1,9 @@
+package model;
+
+public class LogicTileMoveData {
+    private boolean upwards;
+    private boolean leftwards;
+    private byte range;
+    private byte selectedIndex;
+    private byte targetIndex;
+}

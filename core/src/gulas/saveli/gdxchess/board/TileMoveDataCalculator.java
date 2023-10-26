abcc5e8002934379bda2247c05e8718abb59f9f2
saveligulas <saveli.gulas@gmail.com>
2023-10-26 16:52:05 +0200

    added method and TODO

---
+++ core/src/gulas/saveli/gdxchess/board/TileMoveDataCalculator.java
@@ -0,0 +1,9 @@
+package board;
+
+import model.TileMoveData;
+
+public class TileMoveDataCalculator {
+    public TileMoveData build(byte selectionIndex, byte targetIndex) { //TODO should turn params into TileMoveData
+        return null;
+    }
+}

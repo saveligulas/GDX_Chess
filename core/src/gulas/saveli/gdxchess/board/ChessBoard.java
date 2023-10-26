5340b230a9bab53b52818bb4939bf60216ba14f1
saveligulas <saveli.gulas@gmail.com>
2023-10-26 16:24:55 +0200

    implemented performMove method and added new field

--- core/src/gulas/saveli/gdxchess/board/ChessBoard.java
+++ core/src/gulas/saveli/gdxchess/board/ChessBoard.java
@@ -1,6 +1,12 @@
 package board;
 
 import lombok.Data;
+import model.Piece;
+
+import java.sql.Array;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class ChessBoard {
@@ -8,11 +14,18 @@ public class ChessBoard {
 
     private boolean moveOrderWhite = true;
 
+    private List<Piece> removedPieces = new ArrayList<>();
+
     public ChessBoard() {
         this.logicTiles = LogicTile.getStandard64Tiles();
     }
 
-    public void performMove(LogicTile tileWithPieceToMove, LogicTile targetedTile) {
-
+    public void performMove(byte selectedIndex, byte targetedIndex) {
+        Piece selectedPiece = logicTiles[selectedIndex].getPieceOnTile();
+        if (logicTiles[targetedIndex].getPieceOnTile() != null) {
+            removedPieces.add(logicTiles[targetedIndex].getPieceOnTile());
+        }
+        logicTiles[targetedIndex].setPieceOnTile(selectedPiece);
+        logicTiles[selectedIndex].setPieceOnTile(null);
     }
 }
